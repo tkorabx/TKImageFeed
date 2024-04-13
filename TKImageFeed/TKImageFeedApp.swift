16f6e4c9048f575becc702f9463044406dbc4635
Tomasz Korab <tomasz.korab95@gmail.com>
2024-04-13 15:06:59 +0200

    Initial Commit

---
+++ TKImageFeed/TKImageFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKImageFeedApp.swift
+//  TKImageFeed
+//
+//  Created by Tomasz Korab on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TKImageFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
